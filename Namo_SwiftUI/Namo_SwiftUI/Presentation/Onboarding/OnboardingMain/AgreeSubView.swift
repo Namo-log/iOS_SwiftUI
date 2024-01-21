6285b5d78cdea2d8f1e2d0d67233ad79ab36f8fe
goghrf <daynnite9924@gmail.com>
2024-01-21 23:32:44 +0900

    add: asset 이미지 아이콘 추가

---
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Onboarding/OnboardingMain/AgreeSubView.swift
@@ -0,0 +1,18 @@
+//
+//  AgreeSubView.swift
+//  Namo_SwiftUI
+//
+//  Created by 고성민 on 1/21/24.
+//
+
+import SwiftUI
+
+struct AgreeSubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgreeSubView()
+}
