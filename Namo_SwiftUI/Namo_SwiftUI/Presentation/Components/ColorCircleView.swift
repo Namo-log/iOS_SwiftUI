409ba683991bce00a8ad8285ff9456969fc6b766
FpRaArNkK <mspark1184@naver.com>
2024-01-24 01:11:38 +0900

    Feat: ColorCircleView 추가

---
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Components/ColorCircleView.swift
@@ -0,0 +1,53 @@
+//
+//  CheckCircle.swift
+//  Namo_SwiftUI
+//
+//  Created by 박민서 on 1/24/24.
+//
+
+import SwiftUI
+
+enum ColorCirclePresentMode {
+    /// 현재 선택 가능한 상태
+    case available
+    /// 현재 선택된 상태
+    case selected
+    /// 현재 선택 불가능한 상태
+    case unAvailable
+}
+
+struct ColorCircleView: View {
+    
+    /// Cilrcle의 지정색입니다.
+    var color: Color?
+    /// Circle의 현재 상태입니다.
+    var selectState: ColorCirclePresentMode?
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Circle()
+                .fill(color ?? .mainText)
+                .aspectRatio(1.0, contentMode: .fit)
+            
+            if (selectState == .unAvailable || selectState == .selected) {
+                Circle()
+                    .fill(.white)
+                    .aspectRatio(0.7, contentMode: .fit)
+            }
+            
+            if (selectState == .selected) {
+                Circle()
+                    .fill(color ?? .mainText)
+                    .aspectRatio(0.55, contentMode: .fit)
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ColorCircleView(color: .cyan, selectState: .selected)
+}
