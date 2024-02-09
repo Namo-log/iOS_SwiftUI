3734a04bdab4e7c241080d7dfa251ccff1d61ec2
goghrf <daynnite9924@gmail.com>
2024-02-09 12:32:20 +0900

    add: asset 이미지 추가

---
+++ Namo_SwiftUI/Namo_SwiftUI/Data/Local/KeyChainManager.swift
@@ -0,0 +1,8 @@
+//
+//  KeyChainManager.swift
+//  Namo_SwiftUI
+//
+//  Created by 고성민 on 2/9/24.
+//
+
+import Foundation
---
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Login/LoginBtn.swift
@@ -0,0 +1,18 @@
+//
+//  LoginBtn.swift
+//  Namo_SwiftUI
+//
+//  Created by 고성민 on 2/9/24.
+//
+
+import SwiftUI
+
+struct LoginBtn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginBtn()
+}
