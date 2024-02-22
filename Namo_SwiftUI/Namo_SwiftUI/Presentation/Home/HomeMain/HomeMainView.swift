33118142cc4015946f7e39a607980f490034a0fc
FpRaArNkK <mspark1184@naver.com>
2024-02-23 02:18:06 +0900

    Feat: HomeMainView - ToDoCreateView 연결 코드 추가

--- Namo_SwiftUI/Namo_SwiftUI/Presentation/Home/HomeMain/HomeMainView.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Home/HomeMain/HomeMainView.swift
@@ -20,77 +20,88 @@ struct HomeMainView: View {
 	@State var datePickerCurrentDate: Date = Date()
 	@State var pickerCurrentYear: Int = Date().toYMD().year
 	@State var pickerCurrentMonth: Int = Date().toYMD().month
+    @State var isToDoSheetPresented: Bool = false
 	
 	let weekdays: [String] = ["일", "월", "화", "수", "목", "금", "토"]
 	
 	var body: some View {
-		ZStack {
-			VStack(spacing: 0) {
-				header
-					.padding(.bottom, 22)
-				
-				weekday
-					.padding(.bottom, 11)
-				
-				CalendarView(calendarController) { date in
-					CalendarItem(date: date, focusDate: $focusDate, calendarSchedule: $calendarSchedule)
-				}
-				.frame(width: screenWidth-20)
-				.padding(.leading, 14)
-				.padding(.trailing, 6)
-				.padding(.bottom, 20)
-				
-				if focusDate != nil {
-					detailView
-						.clipShape(RoundedCorners(radius: 15, corners: [.topLeft, .topRight]))
-						.shadow(color: .black.opacity(0.15), radius: 12, x: 0, y: 0)
-				} else {
-					Spacer(minLength: 0)
-						.frame(height: tabBarHeight)
-				}
-				
-			}
-			
-			if showDatePicker {
-				NamoAlertView(
-					showAlert: $showDatePicker,
-					content: AnyView(
-						HStack(spacing: 0) {
-							Picker("", selection: $pickerCurrentYear) {
-								ForEach(2000...2099, id: \.self) {
-									Text("\(String($0))년")
-										.font(.pretendard(.regular, size: 23))
-								}
-							}
-							.pickerStyle(.inline)
-							
-							Picker("", selection: $pickerCurrentMonth) {
-								ForEach(1...12, id: \.self) {
-									Text("\(String($0))월")
-										.font(.pretendard(.regular, size: 23))
-								}
-							}
-							.pickerStyle(.inline)
-						}
-						.frame(height: 154)
-					),
-					leftButtonTitle: "취소",
-					leftButtonAction: {
-						pickerCurrentYear = calendarController.yearMonth.year
-						pickerCurrentMonth = calendarController.yearMonth.month
-					},
-					rightButtonTitle: "확인",
-					rightButtonAction: {
-						calendarController.scrollTo(YearMonth(year: pickerCurrentYear, month: pickerCurrentMonth))
-					}
-				)
-			}
-		}
+        ZStack {
+            VStack(spacing: 0) {
+                header
+                    .padding(.bottom, 22)
+                
+                weekday
+                    .padding(.bottom, 11)
+                
+                CalendarView(calendarController) { date in
+                    CalendarItem(date: date, focusDate: $focusDate, calendarSchedule: $calendarSchedule)
+                }
+                .frame(width: screenWidth-20)
+                .padding(.leading, 14)
+                .padding(.trailing, 6)
+                .padding(.bottom, 20)
+                
+                if focusDate != nil {
+                    detailView
+                        .clipShape(RoundedCorners(radius: 15, corners: [.topLeft, .topRight]))
+                        .shadow(color: .black.opacity(0.15), radius: 12, x: 0, y: 0)
+                } else {
+                    Spacer(minLength: 0)
+                        .frame(height: tabBarHeight)
+                }
+                
+            }
+            
+            if showDatePicker {
+                NamoAlertView(
+                    showAlert: $showDatePicker,
+                    content: AnyView(
+                        HStack(spacing: 0) {
+                            Picker("", selection: $pickerCurrentYear) {
+                                ForEach(2000...2099, id: \.self) {
+                                    Text("\(String($0))년")
+                                        .font(.pretendard(.regular, size: 23))
+                                }
+                            }
+                            .pickerStyle(.inline)
+                            
+                            Picker("", selection: $pickerCurrentMonth) {
+                                ForEach(1...12, id: \.self) {
+                                    Text("\(String($0))월")
+                                        .font(.pretendard(.regular, size: 23))
+                                }
+                            }
+                            .pickerStyle(.inline)
+                        }
+                            .frame(height: 154)
+                    ),
+                    leftButtonTitle: "취소",
+                    leftButtonAction: {
+                        pickerCurrentYear = calendarController.yearMonth.year
+                        pickerCurrentMonth = calendarController.yearMonth.month
+                    },
+                    rightButtonTitle: "확인",
+                    rightButtonAction: {
+                        calendarController.scrollTo(YearMonth(year: pickerCurrentYear, month: pickerCurrentMonth))
+                    }
+                )
+            }
+            
+            if isToDoSheetPresented {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea(.all, edges: .all)
+            }
+            
+        }
 		.ignoresSafeArea(edges: .bottom)
 		.task {
 			self.calendarSchedule = await scheduleInteractor.setCalendar()
 			await categoryInteractor.getCategories()
 		}
+        .fullScreenCover(isPresented: $isToDoSheetPresented, content: {
+            ToDoCreateView(schedule: nil)
+                .background(ClearBackground())
+        })
 	}
 	
 	private var header: some View {
@@ -190,7 +201,9 @@ struct HomeMainView: View {
 		.frame(width: screenWidth, height: screenHeight * 0.47)
 		.background(Color.white)
 		.overlay(alignment: .bottomTrailing) {
-			Button(action: {}, label: {
+			Button(action: {
+                self.isToDoSheetPresented = true
+            }, label: {
 				Image(.floatingAdd)
 					.padding(.bottom, 37)
 					.padding(.trailing, 25)
