a0350d9b4f4ffb3e2542da288d2dc6eeeeff4a80
EunsuSeo01 <eunsu0613@naver.com>
2024-02-26 03:53:43 +0900

    Feat: 개인 / 모임 탭 기능 구현

--- Namo_SwiftUI/Namo_SwiftUI/Presentation/Diary/DiaryMain/DiaryMainView.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Diary/DiaryMain/DiaryMainView.swift
@@ -15,11 +15,47 @@ struct DiaryMainView: View {
     @State var pickerCurrentYear: Int = Date().toYMD().year
     @State var pickerCurrentMonth: Int = Date().toYMD().month
     
-    @State var dummyDiary: [Int] = [1,2]
+    @State var dummyDiary = [DiaryItem(backgroundColor: .mainOrange, scheduleName: "점심 약속",
+                                       content: "팀 빌딩을 앞두고 PM들끼리 모였다! 네오가 주도하셨는데, 밥 먹고 이디야에 가서 아이디어 피드백을 주고받았다. 원래 막막했었는데 도움이 많이 되었다. 팀 빌딩... 지원이 많이많이 들어왔으면 좋겠다."),
+                             DiaryItem(backgroundColor: .mainOrange, scheduleName: "점심 약속",
+                                       content: "팀 빌딩을 앞두고 PM들끼리 모였다! 네오가 주도하셨는데, 밥 먹고 이디야에 가서 아이디어 피드백을 주고받았다. 원래 막막했었는데 도움이 많이 되었다. 팀 빌딩... 지원이 많이많이 들어왔으면 좋겠다.")]
+    @State var dummyGroupDiary = [DiaryItem(backgroundColor: .pink, scheduleName: "데모데이",
+                                            content: "앱 런칭 데모데이를 진행했다. 참여진 분들과 피드백도 주고 받고, 무엇보다 우리 팀이 열심히 제작한 나모 앱을 누군가에게 보여줄 수 있다는 점이 뿌듯했다 ! 부스 정리하고 회식까지 하니 정말 다 끝난느낌...")]
+    
+    @State var isPersonal = true
+    
+    // 개인 / 모임 토글
+    private var toggleView: some View {
+        Capsule()
+            .foregroundColor(.mainGray)
+            .frame(width: 152, height: 30)
+            .overlay(alignment: isPersonal ? .leading : .trailing, content: {
+                Capsule()
+                    .fill(.mainOrange)
+                    .frame(width: 80, height: 26)
+                    .overlay(
+                        Text(isPersonal ? "개인" : "모임")
+                            .font(.pretendard(.bold, size: 15))
+                            .foregroundStyle(.white)
+                    )
+                    .padding(.leading, 2)
+                    .padding(.trailing, 2)
+            })
+            .overlay(alignment: isPersonal ? .trailing : .leading, content: {
+                Text(isPersonal ? "모임" : "개인")
+                    .font(.pretendard(.bold, size: 15))
+                    .foregroundStyle(Color(.mainText))
+                    .padding(isPersonal ? .trailing : .leading, 24)
+                    .onTapGesture {
+                        isPersonal.toggle()
+                    }
+            })
+    }
     
     var body: some View {
         ZStack {
             VStack {
+                // 헤더
                 HStack {
                     Button {
                         showDatePicker = true
@@ -35,42 +71,21 @@ struct DiaryMainView: View {
                     
                     Spacer()
                     
-                    // TODO: - 탭 이동
-                    Capsule()
-                        .foregroundColor(.mainGray)
-                        .frame(width: 152, height: 30)
-                        .overlay(alignment: .leading, content: {
-                            Capsule()
-                                .fill(.mainOrange)
-                                .frame(width: 80, height: 26)
-                                .overlay(
-                                    Text("개인")
-                                        .font(.pretendard(.bold, size: 15))
-                                        .foregroundStyle(.white)
-                                )
-                                .padding(.leading, 2)
-                                .padding(.trailing, 2)
-                        })
-                        .overlay(alignment: .trailing, content: {
-                            Text("모임")
-                                .font(.pretendard(.bold, size: 15))
-                                .foregroundStyle(Color(.mainText))
-                                .padding(.trailing, 24)
-                        })
+                    toggleView
                 }
                 .padding(.leading, 20)
                 .padding(.trailing, 20)
                 
-                if dummyDiary.isEmpty {
+                // 기록 목록
+                if isPersonal ? dummyDiary.isEmpty : dummyGroupDiary.isEmpty {
                     Text("기록이 없습니다. 기록을 추가해 보세요!")
                         .font(.pretendard(.light, size: 15)) // Weight 400 -> .light
                         .padding(.top, 24)
                 } else {
                     VStack(spacing: 20) {
-                        ForEach(dummyDiary, id: \.self) { _ in
+                        ForEach(isPersonal ? dummyDiary : dummyGroupDiary) { diary in
                             DiaryDateItem()
-                            DiaryItem(backgroundColor: .mainOrange, scheduleName: "점심 약속",
-                                      content: "팀 빌딩을 앞두고 PM들끼리 모였다! 네오가 주도하셨는데, 밥 먹고 이디야에 가서 아이디어 피드백을 주고받았다. 원래 막막했었는데 도움이 많이 되었다. 팀 빌딩... 지원이 많이많이 들어왔으면 좋겠다.")
+                            diary
                         }
                     }
                 }
@@ -81,7 +96,7 @@ struct DiaryMainView: View {
                 }
                 
                 Spacer()
-            }
+            } // VStack
             
             if showDatePicker {
                 NamoAlertView(
@@ -117,7 +132,7 @@ struct DiaryMainView: View {
                     }
                 )
             }
-        }
+        } // ZStack
     }
 }
 
@@ -150,7 +165,8 @@ struct DiaryDateItem: View {
 
 // 다이어리 아이템
 // TODO: - 내용에 따라 동적 높이 설정... 어떻게 하지
-struct DiaryItem: View {
+struct DiaryItem: View, Identifiable {
+    let id = UUID()
     let backgroundColor: Color
     let scheduleName: String
     let content: String
