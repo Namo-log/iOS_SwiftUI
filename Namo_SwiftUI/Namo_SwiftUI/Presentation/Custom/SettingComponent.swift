29b9b993b70e7fc2b406c2026b2e94f58d5d0797
goghrf <daynnite9924@gmail.com>
2024-02-26 14:04:15 +0900

    add: asset 이미지 추가

---
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Custom/SettingComponent.swift
@@ -0,0 +1,18 @@
+//
+//  SettingComponent.swift
+//  Namo_SwiftUI
+//
+//  Created by 고성민 on 2/24/24.
+//
+
+import SwiftUI
+
+struct SettingComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingComponent()
+}
---
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Custom/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Namo_SwiftUI
+//
+//  Created by 고성민 on 2/24/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
