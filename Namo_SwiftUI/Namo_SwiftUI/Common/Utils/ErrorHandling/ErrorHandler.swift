914642c8ca0bdefa714525e1b5626f386a50ba4c
FpRaArNkK <mspark1184@naver.com>
2024-03-13 02:27:21 +0900

    Feat: ErrorHandler - keyWindow, rootController 작성

--- Namo_SwiftUI/Namo_SwiftUI/Common/Utils/ErrorHandling/ErrorHandler.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Common/Utils/ErrorHandling/ErrorHandler.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ErrorHandler {
+    // Singleton
+    static let shared = ErrorHandler()
+    
+    /// Namo 앱이 현재 표시중인 Window입니다.
+    /// App에 연결된 Scene들 중에서 keyWindow를 찾아 return합니다.
+    private var keyWindow: UIWindow? {
+        let allScenes = UIApplication.shared.connectedScenes
+        for scene in allScenes {
+            guard let windowScene = scene as? UIWindowScene else { continue }
+            for window in windowScene.windows where window.isKeyWindow {
+                return window
+            }
+        }
+        return nil
+    }
+    
+    /// Namo 앱의 rootViewController입니다.
+    /// keyWindow의 rootViewController를 받아 앱에서 사용가능한 presentedViewController를 return 합니다.
+    private var rootController: UIViewController? {
+        var root = keyWindow?.rootViewController
+        while let presentedViewController = root?.presentedViewController {
+            root = presentedViewController
+        }
+        return root
+    }
+    
+    
+}
