421bf9d5af016ea2fbb0ba104f92474438b4628d
FpRaArNkK <mspark1184@naver.com>
2024-03-13 02:16:57 +0900

    Feat: AppError, ErrorHandler init

---
+++ Namo_SwiftUI/Namo_SwiftUI/Common/Utils/ErrorHandling/AppError.swift
@@ -0,0 +1,8 @@
+//
+//  AppError.swift
+//  Namo_SwiftUI
+//
+//  Created by 박민서 on 3/13/24.
+//
+
+import Foundation
---
+++ Namo_SwiftUI/Namo_SwiftUI/Common/Utils/ErrorHandling/ErrorHandler.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorHandler.swift
+//  Namo_SwiftUI
+//
+//  Created by 박민서 on 3/13/24.
+//
+
+import Foundation
