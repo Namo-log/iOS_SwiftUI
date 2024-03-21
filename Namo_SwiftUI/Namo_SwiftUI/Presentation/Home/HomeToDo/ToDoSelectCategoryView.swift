20b5c2290727195efc813fbc9cbe18b77fb649d5
goghrf <daynnite9924@gmail.com>
2024-03-21 17:25:22 +0900

    Feat: 기존 뷰에 카테고리 편집 로직 추가

--- Namo_SwiftUI/Namo_SwiftUI/Presentation/Home/HomeToDo/ToDoSelectCategoryView.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Home/HomeToDo/ToDoSelectCategoryView.swift
@@ -18,38 +18,46 @@ struct ToDoSelectCategoryView: View {
     
     /// 화면에 표시되기 위한 categoryList State
     @State private var categoryList: [ScheduleCategory] = []
-    /// 현재화면 dismiss
-    @Environment(\.dismiss) private var dismiss
+    
+    /// NavigationPath
+    @Binding var path: NavigationPath
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    //MARK: 카테고리 목록
-                    ForEach(categoryList, id: \.self) { category in
-                        HStack {
-                            ColorCircleView(color: category.color, selectState: category.isSelected ?? false ? .checked : .available)
-                                .frame(width: 20, height: 20)
-                            Text(category.name)
-                                .font(.pretendard(.regular, size: 15))
-                                .foregroundStyle(.mainText)
-                            Spacer()
-                        }
-                        .lineSpacing(12)
-                        .onTapGesture {
-                            for index in categoryList.indices {
-                                if  categoryList[index].hashValue == category.hashValue {
-                                    categoryList[index].isSelected = true
-                                    self.appState.scheduleState.scheduleTemp.categoryId = category.categoryId
-                                } else {
-                                    categoryList[index].isSelected = false
-                                }
-                            }
-                            dismiss()
-                        }
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                
+                //MARK: 카테고리 목록
+                ForEach(categoryList, id: \.self) { category in
+                    HStack {
+                        ColorCircleView(color: category.color, selectState: category.isSelected ?? false ? .checked : .available)
+                            .frame(width: 20, height: 20)
+                        Text(category.name)
+                            .font(.pretendard(.regular, size: 15))
+                            .foregroundStyle(.mainText)
+                        Spacer()
                     }
+                    .lineSpacing(12)
+                    .onTapGesture {
+                        for index in categoryList.indices {
+                            if  categoryList[index].hashValue == category.hashValue {
+                                categoryList[index].isSelected = true
+                                self.appState.scheduleState.scheduleTemp.categoryId = category.categoryId
+                            } else {
+                                categoryList[index].isSelected = false
+                            }
+                        }
+                        // 뒤로 가기
+                        path.removeLast()
+                    }
+                }
+                
+                //MARK: "카테고리 편집"
+                
+                Button {
                     
-                    //MARK: "카테고리 편집"
+                    path.append(CategoryViews.CategorySettingView)
+                    
+                } label: {
                     HStack {
                         Image(systemName: "square.grid.2x2")
                             .renderingMode(.template)
@@ -59,42 +67,33 @@ struct ToDoSelectCategoryView: View {
                             .foregroundStyle(.mainText)
                     }
                     .lineSpacing(12)
-        
-                }//: VStack
-                .padding(EdgeInsets(top: 10, leading: 30, bottom: 20, trailing: 30))
-                .navigationTitle("카테고리")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarBackButtonHidden(true) // Back버튼 지우기
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button(action: {
-                            dismiss()
-                        }, label: {
-                            Text("닫기")
-                                .font(.pretendard(.regular, size: 15))
-                        })
-                        .foregroundStyle(.mainText)
-                    }
-                }//: toolbar
-            }//: ScrollView
-        }//: NavigationStack
-        .onAppear {
-            // 카테고리 최신화 후, 카테고리 리스트에 반영
-            Task {
-                await self.categoryInteractor.getCategories()
-                self.categoryList = self.appState.categoryState.categoryList.map { category in
-                    
-                    return ScheduleCategory(
-                        categoryId: category.categoryId,
-                        name: category.name,
-                        paletteId: category.paletteId,
-                        isShare: category.isShare,
-                        color: categoryInteractor.getColorWithPaletteId(id: category.paletteId),
-                        isSelected: self.appState.scheduleState.scheduleTemp.categoryId == category.categoryId ? true : false
-                    )
-                    
                 }
-            }
+                
+                Rectangle()
+                    .foregroundStyle(.clear)
+                    .frame(height: 100)
+                
+            }//: VStack
+            .padding(EdgeInsets(top: 10, leading: 30, bottom: 20, trailing: 30))
+            .navigationTitle("카테고리")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true) // Back버튼 지우기
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        // 뒤로 가기
+                        path.removeLast()
+                    }, label: {
+                        Text("닫기")
+                            .font(.pretendard(.regular, size: 15))
+                    })
+                    .foregroundStyle(.mainText)
+                }
+            }//: toolbar
+        }//: ScrollView
+        .onAppear {
+            
+            self.categoryList = self.categoryInteractor.setCategories()
         }
     }
 }
@@ -102,6 +101,6 @@ struct ToDoSelectCategoryView: View {
 
 
 #Preview {
-    ToDoSelectCategoryView()
+    ToDoSelectCategoryView(path: .constant(NavigationPath()))
         .environmentObject(AppState())
 }
