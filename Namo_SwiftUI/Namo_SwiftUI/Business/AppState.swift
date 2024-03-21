064e35ecc237646498c118a06e76b55ae8a74e4d
goghrf <daynnite9924@gmail.com>
2024-03-21 17:18:38 +0900

    feat: category State 변수 추가

--- Namo_SwiftUI/Namo_SwiftUI/Business/AppState.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Business/AppState.swift
@@ -16,6 +16,13 @@ struct CategoryState {
     var categoryList: [ScheduleCategory]
 }
 
+struct CategoryInputState {
+    
+    var categoryTitle: String
+    var selectedPaletteId: Int
+    var isShare: Bool
+}
+
 struct PlaceState {
     var placeList: [Place]
     var selectedPlace: Place?
@@ -64,9 +71,19 @@ class AppState: ObservableObject {
     @Published var placeState: PlaceState = PlaceState(
         placeList: [], selectedPlace: nil
     )
-
-    // categoryId : name
-//    @Published var categoryName: [Int: String] = [:]
     
-//    @Published var categoryList: [CategoryDTO] = []
+    // 카테고리 삭제 버튼 보이기
+    @Published var showCategoryDeleteBtn: Bool = false
+    
+    // 카테고리 삭제 불가 여부
+    @Published var categoryCantDelete: Bool = false
+    
+    // 카테고리 삭제 완료 토글 보이기
+    @Published var showCategoryDeleteDoneToast: Bool = false
+    
+    // 카테고리 수정 완료 토글 보이기
+    @Published var showCategoryEditDoneToast: Bool = false
+    
+    // 카테고리 삭제 불가 토글 보이기
+    @Published var showCategoryCantDeleteToast: Bool = false
 }
