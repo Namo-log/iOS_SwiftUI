07c5a301662f8ce8b931926b2055449c6e4fcd31
KoSungmin <goghrf@KoSungminui-MacBookPro.local>
2024-05-03 17:09:07 +0900

    Chore: SplashView 주석 추가

--- Namo_SwiftUI/Namo_SwiftUI/App/SplashView.swift
+++ Namo_SwiftUI/Namo_SwiftUI/App/SplashView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct SplashView: View {
     
     @State var isActive: Bool = false   // 다음 화면 활성화
-    @AppStorage("onboardingDone") var onboardingDone: Bool = false
-    @AppStorage("isLogin") var isLogin: Bool = false
-    @AppStorage("newUser") var newUser: Bool = true
+    @AppStorage("onboardingDone") var onboardingDone: Bool = false  // 온보딩 완료 여부
+    @AppStorage("isLogin") var isLogin: Bool = false                // 로그인 여부
+    @AppStorage("newUser") var newUser: Bool = true                 // 약관동의 완료 여부
     
     var socialLogin: String?
     
@@ -20,15 +20,15 @@ struct SplashView: View {
         ZStack {
             
             if !onboardingDone && isActive {
-                OnboardingView()
+                OnboardingView()    // 온보딩 화면
             } else if onboardingDone && isActive && !isLogin {
-                LoginView()
+                LoginView()         // 로그인 화면
             } else if onboardingDone && isActive && isLogin && !newUser {
-                NamoHome()
+                NamoHome()          // 나모 홈 화면
             } else if onboardingDone && isActive && isLogin && newUser {
-                AgreeMainView()
+                AgreeMainView()     // 약관동의 화면
             } else {
-                ZStack {
+                ZStack {            // 스플래시 화면
                     
                     LinearGradient(gradient: Gradient(colors: [Color(hex: 0xe59744), Color.mainOrange]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     
