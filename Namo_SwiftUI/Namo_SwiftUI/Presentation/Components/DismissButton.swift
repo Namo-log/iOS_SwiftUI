d8cf59abf58f04afadbd3306f7cc3ae7bffc191f
FpRaArNkK <mspark1184@naver.com>
2024-06-01 17:12:27 +0900

    Fix: 기록 편집 해제 후 기록 삭제 안되는 이슈 해결

--- Namo_SwiftUI/Namo_SwiftUI/Presentation/Components/DismissButton.swift
+++ Namo_SwiftUI/Namo_SwiftUI/Presentation/Components/DismissButton.swift
@@ -6,15 +6,21 @@
 //
 
 import SwiftUI
+import Factory
 
 // 네비게이션 왼쪽 아이템으로 쓰이는 주황색 뒤로가기 버튼
 struct DismissButton: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @EnvironmentObject var scheduleState: ScheduleState
+    @Injected(\.scheduleInteractor) var scheduleInteractor
     @Binding var isDeletingDiary: Bool
     
     var body: some View {
         Button{
             self.presentationMode.wrappedValue.dismiss()
+//            scheduleState.currentSchedule = 
+            scheduleInteractor.setScheduleToCurrentSchedule(schedule: nil)
+            scheduleInteractor.setScheduleToCurrentMoimSchedule(schedule: nil, users: nil)
         } label: {
             Image(.icBack)
                 .resizable()
