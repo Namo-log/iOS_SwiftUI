e27bec8e32b722555dae7001d35475650683740d
Seokki-Kwon <seokki0706@gmail.com>
2024-08-14 01:20:07 +0900

    Feat: 기존의 BuildSetting 마이그레이션

--- Namo_SwiftUI/Project.swift
+++ Namo_SwiftUI/Project.swift
@@ -3,11 +3,15 @@ import ProjectDescription
 
 let project = Project(
     name: "Namo_SwiftUI",
+    settings: .settings(configurations: [
+        .debug(name: "Debug", xcconfig: "Configurations/Namo_SwiftUI-debug.xcconfig"),
+        .release(name: "Release", xcconfig: "Configurations/Namo_SwiftUI-release.xcconfig")
+    ]),
     targets: [
         .target(name: "Namo_SwiftUI",
                 destinations: .iOS,
                 product: .app,
-                bundleId: "",
+                bundleId: "com.mongmong.namo.test",
                 sources: ["Namo_SwiftUI/Sources/**"],
                 resources: ["Namo_SwiftUI/Resources/**"],
                 dependencies: []
