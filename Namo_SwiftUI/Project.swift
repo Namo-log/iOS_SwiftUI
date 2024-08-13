095fd2c91f4aef7fd223a65a20ce834ff67cc2f2
Seokki-Kwon <seokki0706@gmail.com>
2024-08-14 02:14:40 +0900

    Feat: 라이브러리 의존성 추가

--- Namo_SwiftUI/Project.swift
+++ Namo_SwiftUI/Project.swift
@@ -14,7 +14,23 @@ let project = Project(
                 bundleId: "com.mongmong.namo.test",
                 sources: ["Namo_SwiftUI/Sources/**"],
                 resources: ["Namo_SwiftUI/Resources/**"],
-                dependencies: []
+                dependencies: [
+                    .external(name: "Kingfisher"),
+                    .external(name: "Factory"),
+                    .external(name: "Alamofire"),
+                    .external(name: "Lottie"),
+                    .external(name: "SwiftUICalendar"),
+                    .external(name: "naveridlogin-ios-sp"),
+                    .external(name: "KakaoMapsSDK-SPM"),
+                    .external(name: "KakaoSDK"),
+                    .external(name: "FirebaseDatabase"),
+                    .external(name: "FirebaseCrashlytics"),
+                        .external(name: "FirebaseDynamicLinks"),
+                        .external(name: "FirebaseMessaging"),
+                        .external(name: "FirebasePerformance"),
+                        .external(name: "FirebaseRemoteConfig"),
+                        .external(name: "FirebaseAnalytics"),
+                ]
                )
     ]
 )
--- Namo_SwiftUI/Tuist/Package.swift
+++ Namo_SwiftUI/Tuist/Package.swift
@@ -18,5 +18,14 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.8.1")),
+        .package(url: "https://github.com/hmlongco/Factory", .upToNextMajor(from: "2.3.1")),
+        .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: "7.10.2")),
+        .package(url: "https://github.com/airbnb/lottie-ios", .upToNextMajor(from: "4.3.4")),
+        .package(url: "https://github.com/GGJJack/SwiftUICalendar", .upToNextMajor(from: "0.1.14")),
+        .package(url: "https://github.com/kyungkoo/naveridlogin-ios-sp", .upToNextMajor(from: "4.1.5")),
+        .package(url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM", .upToNextMajor(from: "2.6.3")),
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", .upToNextMajor(from: "2.20.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "10.27.0")),
     ]
 )
