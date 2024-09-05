8c44dfa8bfac2ea4714dfa12cd8012cb527dba4c
Seokki-Kwon <>
2024-09-05 17:08:00 +0900

    Feat: Moim 모듈 설정 및 생성

---
+++ Namo_SwiftUI/Projects/Feature/Moim/Project.swift
@@ -0,0 +1,62 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by 권석기 on 9/5/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let targets: [Target] = [
+    .feature(
+        interface: .Moim,
+        factory: .init(
+            dependencies: [
+                .domain
+            ]
+        )
+    ),
+    .feature(
+        implements: .Moim,
+        factory: .init(
+            dependencies: [
+                .feature(interface: .Moim)
+            ]
+        )
+    ),
+    .feature(
+        testing: .Moim,
+        factory: .init(
+            dependencies: [
+                .feature(interface: .Moim)
+            ]
+        )
+    ),
+    .feature(
+        tests: .Moim,
+        factory: .init(
+            dependencies: [
+                .feature(testing: .Moim)
+            ]
+        )
+    ),
+    .feature(
+        example: .Moim,
+        factory: .init(
+            infoPlist: .exampleAppDefault,
+            dependencies: [
+                .feature(interface: .Moim),
+                .feature(implements: .Moim)
+            ]
+        )
+    )
+]
+
+let project: Project = .makeModule(
+    name: ModulePath.Feature.name+ModulePath.Feature.Moim.rawValue,
+    targets: targets
+)
+
+
