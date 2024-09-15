45dc98f743f8e46fc2e280ca05902e1e70d11b02
FpRaArNkK <mspark1184@naver.com>
2024-09-15 18:28:09 +0900

    Feat: TOSListView 추가

--- Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSView.swift
+++ Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSView.swift
@@ -13,11 +13,13 @@ public struct OnboardingTOSView: View {
     public init() {}
     
     public var body: some View {
-        ZStack {
+        VStack {
             Text("서비스 이용을 위한 ")
                 .font(Font.pretendard(.regular, size: 18))
             + Text("약관 동의")
                 .font(Font.pretendard(.bold, size: 18))
+            TOSListView()
         }
+        .padding(.bottom, 40)
     }
 }
