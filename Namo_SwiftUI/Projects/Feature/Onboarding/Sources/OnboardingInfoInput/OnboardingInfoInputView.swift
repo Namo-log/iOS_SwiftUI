54cd976c2d475fdb2a2270ceaee9766a65a24aae
FpRaArNkK <mspark1184@naver.com>
2024-09-16 00:54:34 +0900

    Add: OnboardingInfoInputView, ic_favColor iconSet 추가

---
+++ Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingInfoInput/OnboardingInfoInputView.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingInfoInputView.swift
+//  FeatureOnboarding
+//
+//  Created by 박민서 on 9/16/24.
+//
+
+import SwiftUI
+import SharedDesignSystem
+import SharedUtil
+
+public struct OnboardingInfoInputView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Text("프로필 설정")
+                .font(.pretendard(.regular, size: 20))
+                .foregroundColor(.colorBlack)
+        }
+    }
+}
