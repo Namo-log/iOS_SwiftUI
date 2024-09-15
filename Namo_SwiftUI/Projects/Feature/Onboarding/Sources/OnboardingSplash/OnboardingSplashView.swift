946d5afa709295a98e968bfc6537c3fe8023efa4
FpRaArNkK <mspark1184@naver.com>
2024-09-16 00:33:32 +0900

    Feat: OnboardingSplashView 작성

---
+++ Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingSplash/OnboardingSplashView.swift
@@ -0,0 +1,42 @@
+//
+//  OnboardingSplashView.swift
+//  FeatureOnboarding
+//
+//  Created by 박민서 on 9/16/24.
+//
+
+import SwiftUI
+import SharedDesignSystem
+
+public struct OnboardingSplashView: View {
+    
+    @State var showUpdateRequired: Bool = false
+    
+    public init() {}
+    
+    public var body: some View {
+        ZStack {
+            
+            LinearGradient(
+                gradient: Gradient(
+                    colors: [
+                        Color(hex: 0xe59744),
+                        Color(asset: SharedDesignSystemAsset.Assets.mainOrange)
+                    ]
+                ),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            
+            Image(asset: SharedDesignSystemAsset.Assets.logo)
+        }
+        .namoAlertView(
+            isPresented: $showUpdateRequired,
+            title: "업데이트가 필요합니다.",
+            content: "업데이트가 필요합니다.",
+            confirmAction: {
+                // appStore 열기
+            }
+        )
+    }
+}
