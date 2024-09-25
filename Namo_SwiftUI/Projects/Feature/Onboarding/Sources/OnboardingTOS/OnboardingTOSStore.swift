ab3e7be4057d4d5a210b8b5cbad287a91658e93b
FpRaArNkK <mspark1184@naver.com>
2024-09-26 04:49:05 +0900

    Feat: OnboardingTOSStore - 약관 동의 여부 버튼 상태 반영 로직 추가

--- Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSStore.swift
+++ Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSStore.swift
@@ -25,6 +25,8 @@ public struct OnboardingTOSStore {
         var locationServiceAgreement: Bool = false
         /// 앱 푸쉬 알림 수신 동의
         var pushNotificationConsent: Bool = false
+        /// 확인 버튼 활성화 상태
+        var nextButtonIsEnabled: Bool = false
         
         public init() {
             // 체크 로직 수행
@@ -61,8 +63,10 @@ public struct OnboardingTOSStore {
                 case .personalDataConsent:
                     state.personalDataConsent.toggle()
                 case .locationServiceAgreement:
+                    // TODO: 위치 허용 부분 추후 추가
                     state.locationServiceAgreement.toggle()
                 case .pushNotificationConsent:
+                    // TODO: 푸쉬 알림 허용 부분 추후 추가
                     state.pushNotificationConsent.toggle()
                 }
                 
@@ -81,16 +85,13 @@ public struct OnboardingTOSStore {
                 return .none
                 
             case .checkAgreements:
+                // 필수 약관 동의 여부 판단
+                let requiredAgreementsChecked: Bool = state.termsOfServiceAgreement && state.personalDataConsent
                 // 모든 약관의 전체 동의 여부 판단
-                let allAgreementsChecked: Bool =
-                ![
-                    state.termsOfServiceAgreement,
-                    state.personalDataConsent,
-                    state.locationServiceAgreement,
-                    state.pushNotificationConsent
-                ].contains(false)
-                
+                let allAgreementsChecked: Bool = requiredAgreementsChecked && state.locationServiceAgreement && state.pushNotificationConsent
+                // 약관 동의 여부 반영
                 state.entireAgreement = allAgreementsChecked
+                state.nextButtonIsEnabled = requiredAgreementsChecked
                 return .none
             }
         }
--- Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSView.swift
+++ Namo_SwiftUI/Projects/Feature/Onboarding/Sources/OnboardingTOS/OnboardingTOSView.swift
@@ -18,27 +18,29 @@ public struct OnboardingTOSView: View {
     }
     
     public var body: some View {
-        VStack(spacing: 56) {
-            
-            Spacer()
-            
-            Text("서비스 이용을 위한 ")
-                .foregroundColor(.colorBlack)
-                .font(Font.pretendard(.regular, size: 18))
-            + Text("약관 동의")
-                .foregroundColor(.colorBlack)
-                .font(Font.pretendard(.bold, size: 18))
-            
-            TOSListView(store: store)
-            
-            Spacer()
-            
-            NamoButton(title: "확인", type: .active, action: {
-                print("확인")
-            })
-            .padding(.horizontal, 25)
-            
+        WithPerceptionTracking {
+            VStack(spacing: 56) {
+                
+                Spacer()
+                
+                Text("서비스 이용을 위한 ")
+                    .foregroundColor(.colorBlack)
+                    .font(Font.pretendard(.regular, size: 18))
+                + Text("약관 동의")
+                    .foregroundColor(.colorBlack)
+                    .font(Font.pretendard(.bold, size: 18))
+                
+                TOSListView(store: store)
+                
+                Spacer()
+                
+                NamoButton(title: "확인", type: store.nextButtonIsEnabled ? .active : .inactive, action: {
+                    print("확인")
+                })
+                .padding(.horizontal, 25)
+                
+            }
+            .padding(.bottom, 40)
         }
-        .padding(.bottom, 40)
     }
 }
