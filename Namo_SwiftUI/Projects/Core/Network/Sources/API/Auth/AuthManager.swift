02a32775ad77b82bea87cb6b3f7ce690ec153177
FpRaArNkK <mspark1184@naver.com>
2024-10-05 00:53:48 +0900

    Feat: AuthManager 로그인 로직 추가

--- Namo_SwiftUI/Projects/Core/Network/Sources/API/Auth/AuthManager.swift
+++ Namo_SwiftUI/Projects/Core/Network/Sources/API/Auth/AuthManager.swift
@@ -6,14 +6,74 @@
 //
 
 import Foundation
+import SharedUtil
 
 public struct AuthManager {
-    // UserDefaults
-    // 카카오/네이버/애플 로그인 상태 저장
-//    UserDefaults.standard.set("kakao", forKey: "socialLogin")
-//    UserDefaults.standard.set("naver", forKey: "socialLogin")
-//    UserDefaults.standard.set("apple", forKey: "socialLogin")
     
+    enum OAuthType: String {
+        case kakao
+        case naver
+        case apple
+    }
+    
+    /// 로그인 상태 가져오기
+    func getLoginState() -> OAuthType? {
+        guard let oAuthTypeString = UserDefaults.standard.string(forKey: "socialLogin") else { return nil }
+        return OAuthType(rawValue: oAuthTypeString)
+    }
+    
+    /// 카카오/네이버/애플 로그인 상태 저장
+    func setLoginState(_ oAuthType: OAuthType) {
+        UserDefaults.standard.set(oAuthType.rawValue, forKey: "socialLogin")
+    }
+    
+//    /// 카카오/네이버/애플 로그아웃 상태 저장
+//    func setLogoutState(with oAuthType: OAuthType) async {
+//        
+//        do {
+//            // refreshToken 가져오기
+//            let refreshToken: String = try KeyChainManager.readItem(key: "refreshToken")
+//            // 나모 로그아웃 API 호출
+//            try await postNamoLogout(refreshToken: refreshToken)
+//            // 로그인 상태 가져오기
+//            if let loginState = getLoginState() {
+//                switch loginState {
+//                    
+//                }
+//            }
+//        } catch {
+//            // 에러 처리
+//        }
+//        
+//        
+//        // "socialLogin" nil 처리
+//        UserDefaults.standard.removeObject(forKey: "socialLogin")
+//        
+//        
+//    }
+//    
+//    /// 나모 로그아웃 API 호출
+//    private func postNamoLogout(refreshToken: String) async throws {
+//        let result: BaseResponse<SignInResponseDTO>? = await APIManager.shared.performRequest(endPoint: AuthEndPoint.logout(refreshToken: LogoutRequestDTO(refreshToken: refreshToken)))
+//        
+//        // 나모 로그아웃 요청이 실패한 경우 에러 throw
+//        if result?.code != 200 {
+//            throw APIError.customError("로그아웃 실패: 응답 코드 \(result?.code ?? 0)")
+//        }
+//    }
+//    
+//    /// OAuthType별 로그아웃 처리
+//    private func logout(with oAuthType: OAuthType) {
+//        switch oAuthType {
+//            
+//        case .kakao:
+//            <#code#>
+//        case .naver:
+//            <#code#>
+//        case .apple:
+//            <#code#>
+//        }
+//    }
     
     // 카카오/네이버/애플 로그아웃 상태 저장
 //    let refreshToken: String = KeyChainManager.readItem(key: "refreshToken")!
