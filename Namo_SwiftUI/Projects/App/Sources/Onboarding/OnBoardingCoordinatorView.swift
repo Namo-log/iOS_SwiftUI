fef9bd614a9820b84fceb082d7c102f9d741deae
FpRaArNkK <mspark1184@naver.com>
2024-10-11 06:20:36 +0900

    Feat: Coordinator 첫 진입 시 버전 체크 수행 로직 작성

--- Namo_SwiftUI/Projects/App/Sources/Onboarding/OnBoardingCoordinatorView.swift
+++ Namo_SwiftUI/Projects/App/Sources/Onboarding/OnBoardingCoordinatorView.swift
@@ -30,7 +30,7 @@ struct OnboardingCoordinatorView: View {
             }
         }
         .onAppear {
-            store.send(.loginCheck)
+            store.send(.initialCheck)
         }
     }
 }
