77cd17f340a2e68d1ccf44ac8ff27ae84b270523
FpRaArNkK <mspark1184@naver.com>
2024-10-11 00:19:24 +0900

    Feat: CLLocationManagerDelegate 관련 로직 작성

--- Namo_SwiftUI/Projects/Shared/Util/Sources/Utils/LocationManager.swift
+++ Namo_SwiftUI/Projects/Shared/Util/Sources/Utils/LocationManager.swift
@@ -8,12 +8,36 @@
 import CoreLocation
 import Combine
 
-final class LocationManager: NSObject, ObservableObject {
+final public class LocationManager: NSObject, ObservableObject {
     private var locationManager = CLLocationManager()
-    
+    /// 유저 위치
     @Published var userLocation: CLLocation?
     
     override init() {
         super.init()
+    }
+}
+
+// MARK: CLLocationManagerDelegate
+extension LocationManager: CLLocationManagerDelegate {
+    
+    // 위치 업데이트 시 호출
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Location Updated: \(String(describing: locations.last))")
+        self.userLocation = locations.last
+    }
+    
+    // 위치 권한 변경 시 호출
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Location Authorized")
+        case .denied, .restricted:
+            print("Location Not Authorized")
+        case .notDetermined:
+            print("Location Authorization Not Determined")
+        default:
+            break
+        }
     }
 }
