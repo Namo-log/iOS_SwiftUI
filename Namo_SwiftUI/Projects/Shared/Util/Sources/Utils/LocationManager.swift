6add7e4e894e681c102bf4ea5d64aa6891828547
FpRaArNkK <mspark1184@naver.com>
2024-10-11 02:01:04 +0900

    Feat: LocationManager 로직 분리, 보강"

--- Namo_SwiftUI/Projects/Shared/Util/Sources/Utils/LocationManager.swift
+++ Namo_SwiftUI/Projects/Shared/Util/Sources/Utils/LocationManager.swift
@@ -9,6 +9,9 @@ import CoreLocation
 import Combine
 
 public protocol LocationManagerProtocol {
+    /// 위치 권한 체크
+    /// 위치 사용 가능 여부를 반환 합니다.
+    func checkLocationAuthorization() -> Bool
     /// 위치 권한 요청
     func requestLocationAuthorization()
     /// 위치 업데이트 요청 - 단발성 업데이트
@@ -38,23 +41,62 @@ final public class LocationManager: NSObject {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
+    
+    /// LocationManager 권한 상태 따른 내부 처리
+    private func handleLocationAuthStatus(_ status: CLAuthorizationStatus) {
+        switch status {
+            
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Location Authorized")
+            return
+        case .denied, .restricted:
+            // TODO: 추후 미허가 따른 처리 필요
+            print("Location Not Authorized")
+            return
+        case .notDetermined:
+            print("Location Authorization Not Determined")
+            locationManager.requestWhenInUseAuthorization() // 권한 요청
+            
+        @unknown default:
+            break
+        }
+    }
 }
 
 // MARK: LocationManagerProtocol
 extension LocationManager: LocationManagerProtocol {
-    /// 위치 정보 퍼블리셔
+    /// 위치 정보 퍼블리셔 제공
     public var userLocationPublisher: AnyPublisher<CLLocation?, Never> {
         $userLocation.eraseToAnyPublisher()
+    }
+    
+    /// 위치 권한 체크
+    /// 위치 사용 가능 여부를 반환 합니다.
+    public func checkLocationAuthorization() -> Bool {
+        // OS에서 해당 앱이 위치 권한이 allow 되어있는지 확인
+        guard CLLocationManager.locationServicesEnabled() else {
+            // TODO: 추후 처리 방법 필요
+            print("시스템 설정에서 위치 서비스 활성화가 필요합니다.")
+            return false
+        }
+        
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return true
+        default:
+            return false
+        }
     }
     
     /// 위치 권한 요청
     public func requestLocationAuthorization() {
         // OS에서 해당 앱이 위치 권한이 allow 되어있는지 확인
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.requestWhenInUseAuthorization() // 위치 권한 허용 요청
-        } else {
-            print("위치 서비스 활성화가 필요합니다.")
+        guard CLLocationManager.locationServicesEnabled() else {
+            print("시스템 설정에서 위치 서비스 활성화가 필요합니다.")
+            return
         }
+        
+        locationManager.requestWhenInUseAuthorization()
     }
     
     /// 위치 업데이트 요청 - 단발성 업데이트
@@ -89,18 +131,8 @@ extension LocationManager: CLLocationManagerDelegate {
     }
     
     // 위치 권한 변경 시 호출
-    // TODO: 권한 따른 처리 필요
     public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus {
-        case .authorizedAlways, .authorizedWhenInUse:
-            print("Location Authorized")
-        case .denied, .restricted:
-            print("Location Not Authorized")
-        case .notDetermined:
-            print("Location Authorization Not Determined")
-        default:
-            break
-        }
+        self.handleLocationAuthStatus(manager.authorizationStatus)
     }
     
     // CLLocationManager 관련 에러 발생시 호출
