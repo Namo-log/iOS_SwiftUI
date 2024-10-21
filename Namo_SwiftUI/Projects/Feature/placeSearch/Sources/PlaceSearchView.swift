6fa68e2f7c1d676764f63f48e68edcd64a9e8b2b
Seokki-Kwon <seokki0706@gmail.com>
2024-10-22 01:11:39 +0900

    Feat: 장소검색뷰 퍼블리싱

--- Namo_SwiftUI/Projects/Feature/placeSearch/Sources/PlaceSearchView.swift
+++ Namo_SwiftUI/Projects/Feature/placeSearch/Sources/PlaceSearchView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SharedDesignSystem
 import ComposableArchitecture
 import FeaturePlaceSearchInterface
 
@@ -19,43 +20,69 @@ public struct PlaceSearchView: View {
     }
     
     public var body: some View {
-        WithPerceptionTracking {            
-            KakaoMapView(store: store, draw: $draw).onAppear(perform: {
-                self.draw = true
-            }).onDisappear(perform: {
-                self.draw = false
-            }).frame(maxWidth: .infinity, maxHeight: 380)
-            
-            
-            HStack {
-                TextField("", text: $store.searchText)
-                    .textFieldStyle(.roundedBorder)
-                Button(action: {
-                    store.send(.searchButtonTapped)
-                }, label: {
-                    Text("검색")
-                })
-            }
-            
-            List(store.searchList, id: \.self.id) { place in
+        WithPerceptionTracking {
+            VStack(spacing: 0) {
+                KakaoMapView(store: store, draw: $draw).onAppear(perform: {
+                    self.draw = true
+                }).onDisappear(perform: {
+                    self.draw = false
+                }).frame(maxWidth: .infinity, maxHeight: 380)
+                
                 VStack {
-                    HStack {
-                        Text(place.placeName)
-                            .font(.title)
+                    HStack(spacing: 32) {
                         
-                        if place.id == store.id {
-                            Text("✅")
+                        VStack {
+                            HStack {
+                                Image(asset: SharedDesignSystemAsset.Assets.icSearchGray)
+                                
+                                TextField("장소 입력", text: $store.searchText)
+                                    .font(.pretendard(.regular, size: 15))
+                                    .foregroundStyle(Color.mainText)
+                            }
+                            
+                            Divider()
                         }
+                        
+                        
+                        Button(action: {
+                            store.send(.searchButtonTapped)
+                        }, label: {
+                            Text("검색")
+                                .font(.pretendard(.bold, size: 15))
+                                .foregroundStyle(.white)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 6)
+                                .background(Color.namoOrange)
+                                .cornerRadius(4)
+                        })
                     }
-                    Text(place.addressName)
+                    .padding(.horizontal, 30)
+                    .padding(.top, 20)
+                    
+                    ScrollView {
+                        LazyVStack(spacing: 20) {
+                            ForEach(store.searchList, id: \.id) { place in
+                                Button(action: {
+                                    store.send(.poiTapped(place.id))
+                                }, label: {
+                                    PlaceCell(placeName: place.placeName,
+                                              placeAddress: place.addressName,
+                                              isSelected: place.id == store.id)
+                                })
+                            }
+                        }
+                        .padding(.horizontal, 30)
+                        .padding(.top, 24)
+                        .padding(.bottom, 8)                        
+                    }
                 }
-                .onTapGesture {
-                    store.send(.poiTapped(place.id))
-                }
+                .background(.white)
+                .clipShape(RoundedCorners(radius: 15, corners: [.topLeft, .topRight]))
+                .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 0)
+                .offset(y: -8)
             }
-            Spacer()
         }
-        .padding(.horizontal, 30)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
---
+++ Namo_SwiftUI/Projects/Feature/placeSearch/Sources/SubViews/PlaceCell.swift
@@ -0,0 +1,52 @@
+//
+//  PlaceCell.swift
+//  FeaturePlaceSearch
+//
+//  Created by 권석기 on 10/22/24.
+//
+
+import SwiftUI
+import SharedDesignSystem
+struct PlaceCell: View {
+    let placeName: String
+    let placeAddress: String
+    let isSelected: Bool
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(placeName)
+                        .font(.pretendard(.bold, size: 14))
+                        .foregroundStyle(Color.mainText)                    
+                    
+                    Text(placeAddress)
+                        .font(.pretendard(.regular, size: 11))
+                        .foregroundStyle(Color.mainText)
+                        .padding(.top, 7)
+                    
+                    Text(placeAddress)
+                        .font(.pretendard(.regular, size: 11))
+                        .foregroundStyle(Color.mainText)
+                        .padding(.top, 5)
+                }
+                
+                Spacer()
+                
+                if isSelected {
+                    Image(asset: SharedDesignSystemAsset.Assets.icCheckPlace)
+                }
+            }
+            .padding(12)
+        }
+        .background(
+            Color.itemBackground
+        )
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 0)
+    }
+}
+
+//#Preview {
+//    PlaceCell()
+//}
